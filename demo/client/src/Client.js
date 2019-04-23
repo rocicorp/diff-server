21a924a0b1c56477074366707d4fe6918f7f2440
Aaron Boodman <aaron@aaronboodman.com>
2019-04-23 12:35:17 -0700

    nwip

--- demo/client/src/Client.js
+++ demo/client/src/Client.js
@@ -17,7 +17,7 @@ class Client extends Component {
             })}
                 <option key='new' value=''>New...</option>
             </select>
-            <textarea style={{width: '100%', height: '15em', fontFamily: 'monospace', whiteSpace: 'pre', marginBottom: '1em'}} value={this.getFunctionText()}/>
+            {this.getFunctionBody()}
             <textarea style={{width: '100%', height: '15em', fontFamily: 'monospace', marginBottom: '1em', background: '#f3f3f3'}} disabled={true}></textarea>
             <div style={{display: 'flex'}}>
                 <div style={{display: 'flex', flexDirection: 'column', flex: 1}}>
@@ -39,11 +39,13 @@ class Client extends Component {
       });
   }
 
-  getFunctionText() {
+  getFunctionBody() {
     if (!this.state.selectedValue) {
-      return null;
+      return <textarea style={{width: '100%', height: '15em', fontFamily: 'monospace', whiteSpace: 'pre', marginBottom: '1em'}}/>
     }
-    return this.props.ops.find(op => op.hash == this.state.selectedValue).code;
+    return <pre style={{display:'inline-block', width: '100%', height: '15em', marginBottom: '1em', border: '1px solid grey', overflow:'auto'}}>
+      {this.props.ops.find(op => op.hash == this.state.selectedValue).code}
+    </pre>
   }
 }
 
