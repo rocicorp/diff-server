ce1525cc28e3cc7b01400d8a2fcf8772fe2a1b23
Aaron Boodman <aaron@aaronboodman.com>
2019-04-23 09:42:25 -0700

    wip

--- demo/client/src/Client.js
+++ demo/client/src/Client.js
@@ -17,9 +17,7 @@ class Client extends Component {
             })}
                 <option key='new' value=''>New...</option>
             </select>
-            <pre style={{width: '100%', height: '15em', fontFamily: 'monospace', marginBottom: '1em', overflow: 'scroll', border: '1px solid grey'}}>
-              {this.getFunctionText()}
-            </pre>
+            <textarea style={{width: '100%', height: '15em', fontFamily: 'monospace', whiteSpace: 'pre', marginBottom: '1em'}} value={this.getFunctionText()}/>
             <textarea style={{width: '100%', height: '15em', fontFamily: 'monospace', marginBottom: '1em', background: '#f3f3f3'}} disabled={true}></textarea>
             <div style={{display: 'flex'}}>
                 <div style={{display: 'flex', flexDirection: 'column', flex: 1}}>
@@ -43,7 +41,7 @@ class Client extends Component {
 
   getFunctionText() {
     if (!this.state.selectedValue) {
-      return '';
+      return null;
     }
     return this.props.ops.find(op => op.hash == this.state.selectedValue).code;
   }
