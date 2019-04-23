85eaf9f378c5b45268ffba55876e825972fc3745
Aaron Boodman <aaron@aaronboodman.com>
2019-04-23 16:16:55 -0700

    hacky hacky hack

--- demo/client/src/Client.js
+++ demo/client/src/Client.js
@@ -5,20 +5,30 @@ class Client extends Component {
     super(props);
     this.state = {
       selectedValue: '',
+      dbState: '',
     };
   }
+
+  componentDidMount() {
+    this.refreshDBState();
+  }
+
   render() {
     return (
         <div>
             <h2>Client {this.props.index}</h2>
-            <select onChange={(e) => this.handleChange_(e)} defaultValue={this.state.selectedValue} style={{width: '100%', marginBottom: '1em'}}>
-            {this.props.ops.map((op, i) => {
-                return <option key={op.hash} value={op.hash}>{getFunctionName(op.code)}</option>
-            })}
+            <select style={{width: '100%', marginBottom: '1em'}} 
+                onChange={(e) => this.handleChange_(e)}
+                defaultValue={this.state.selectedValue}>
+              {this.props.ops.map((op, i) => {
+                  return <option key={op.hash} value={op.hash}>{getFunctionName(op.code)}</option>
+              })}
                 <option key='new' value=''>New...</option>
             </select>
             {this.getFunctionBody()}
-            <textarea style={{width: '100%', height: '15em', fontFamily: 'monospace', marginBottom: '1em', background: '#f3f3f3'}} disabled={true}></textarea>
+            <pre style={{width: '100%', height: '15em', marginBottom: '1em', background: '#f3f3f3', overflow: 'scroll', border: '1px solid grey'}}>
+              {this.state.dbState}
+            </pre>
             <div style={{display: 'flex'}}>
                 <div style={{display: 'flex', flexDirection: 'column', flex: 1}}>
                     <label><input type="checkbox" defaultChecked={true}/>Online</label>
@@ -46,6 +56,15 @@ class Client extends Component {
     return <pre style={{width: '100%', height: '15em', marginBottom: '1em', border: '1px solid grey', overflow:'auto', margin: '1em 0'}}>
       {this.props.ops.find(op => op.hash == this.state.selectedValue).code}
     </pre>
+  }
+
+  async refreshDBState() {
+    const url = `http://localhost:8080/exec?cmd=${escape(`noms show db${this.props.index}::local.value`)}`;
+    fetch(url).then(r => r.text()).then(t => {
+      this.setState({
+        dbState: t,
+      })
+    })
   }
 }
 
