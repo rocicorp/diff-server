e7719e544a76c7161cae0ba0ff29429ef9455f37
Aaron Boodman <aaron@aaronboodman.com>
2019-05-10 00:45:44 -0700

    package renames: cmd -> cli, c -> api

--- api/api.go
+++ api/api.go
@@ -1,3 +1,6 @@
+// package main implements a low-level C API to Replicant
+// suitable for embedding within iOS, Android, desktop
+// software, etc.
 package main
 
 // compile like:
--- c/doc.go
+++
@@ -1,4 +0,0 @@
-// package main implements a low-level C API to Replicant
-// suitable for embedding within iOS, Android, desktop
-// software, etc.
-package main
