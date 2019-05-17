02c2cda908fd357747853e3a46659e8119d348e7
Aaron Boodman <aaron@aaronboodman.com>
2019-05-17 11:20:43 -0700

    Add Android Sample 🎉

---
+++ examples/hello-replicant-android-java/README.md
@@ -0,0 +1,9 @@
+# Hello Replicant (Android/Java)
+
+This example demonstrates using Replicant in an Android/Java app.
+
+To build:
+
+1. Build the Replicant mobile bindings, by following the instructions at [repm/README.md](../../api/repm/README.md).
+2. Open this project in Android Studio.
+3. Run the project.
---
+++ examples/hello-replicant-android-java/app/src/main/java/to/replicate/helloAndroidJava/MainActivity.java
@@ -0,0 +1,45 @@
+package to.replicate.helloAndroidJava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String message = "Error";
+        try {
+            File f = this.getFileStreamPath("db1");
+            repm.Connection conn = repm.Repm.open(f.getAbsolutePath());
+            repm.Command cmd = conn.exec("{\"put\": {\"id\": \"obj1\"}}".getBytes());
+            cmd.write("\"Hello, from Replicant!\"".getBytes());
+            cmd.done();
+
+            cmd = conn.exec("{\"get\": {\"id\": \"obj1\"}}".getBytes());
+            byte[] buf = new byte[1024];
+            long n = cmd.read(buf);
+            message = new String(buf, 0, (int)n);
+        } catch (Exception e) {
+            Log.e("blech", e.toString());
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle("beep")
+                .setMessage(message)
+                .show();
+    }
+
+}
