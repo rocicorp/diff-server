11e711009f517ab53997c581b22949229d2fd48e
Aaron Boodman <aaron@aaronboodman.com>
2019-07-28 12:42:35 -1000

    cleanup

--- db/sync.go
+++ db/sync.go
@@ -27,27 +27,32 @@ func (db *DB) Sync(remote spec.Spec) error {
 	datas.Pull(db.noms, remoteDB.noms, types.NewRef(db.head.Original), progress)
 
 	// 2: Merge client changes into server state
+	// TODO: This will become an RPC to a remote server that will do this step on its side.
 	localHead := db.head
 	rebased, err := remoteDB.handleSync(localHead)
 	if err != nil {
 		return err
 	}
 
+	// 3: Save the new remote state - primarily to avoid re-downloading it in the future and for debugging purposes.
 	_, err = db.noms.SetHead(db.noms.GetDataset(remote_dataset), types.NewRef(rebased.Original))
 	if err != nil {
 		return err
 	}
 
+	// 4: Rebase any new local changes from between 1 and 3.
 	rebased, err = rebase(db, types.NewRef(rebased.Original), db.head)
 	if err != nil {
 		return err
 	}
 
+	// 5: Commit new local head.
 	_, err = db.noms.FastForward(db.noms.GetDataset(local_dataset), db.noms.WriteValue(rebased.Original))
 	return err
 }
 
 func (db *DB) handleSync(commit Commit) (newHead Commit, err error) {
+	// TODO: this probably wants to be global and updated on each sync?
 	cache := history.NewCache(db.noms)
 	err = validate(db, cache, commit)
 	if err != nil {
