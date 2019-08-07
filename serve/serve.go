c6112d1747534c1d15110b5052c46987832749a0
Aaron Boodman <aaron@aaronboodman.com>
2019-08-07 11:43:13 -1000

    Now serving Noms backed by DynamoDB/S3 from Now. Boo. Ya.

--- serve/serve.go
+++ serve/serve.go
@@ -2,11 +2,13 @@ package serve
 
 import (
 	"net/http"
+	"os"
 	"regexp"
 
 	"github.com/attic-labs/noms/go/datas"
 	"github.com/attic-labs/noms/go/nbs"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -15,7 +17,7 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	re, err := regexp.Compile("^/([^/]+)/(.*)")
+	re, err := regexp.Compile("^/serve/([^/]+)/(.*)")
 	chk.NoError(err)
 	parts := re.FindStringSubmatch(r.URL.Path)
 	if parts == nil {
@@ -25,8 +27,9 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	dbName := parts[1]
 
-	sess := session.Must(session.NewSession(aws.NewConfig().WithRegion("us-west-2")))
+	sess := session.Must(session.NewSession(aws.NewConfig().WithRegion("us-west-2").WithCredentials(
+		credentials.NewStaticCredentials(os.Getenv("REPLICANT_AWS_ACCESS_KEY_ID"), os.Getenv("REPLICANT_AWS_SECRET_ACCESS_KEY"), ""))))
 	cs := nbs.NewAWSStore("replicant", dbName, "aa-replicant", s3.New(sess), dynamodb.New(sess), 1<<28)
-	router := datas.Router(cs, "/"+dbName)
+	router := datas.Router(cs, "/serve/"+dbName)
 	router.ServeHTTP(w, r)
 }
