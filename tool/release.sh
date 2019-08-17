2922caf6f1e6b1e3376757ce20e54b6fb40e2e4e
Aaron Boodman <aaron@aaronboodman.com>
2019-08-17 01:08:48 -1000

    Add a quick release script

---
+++ tool/release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ORIG=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT=$DIR/../
+cd $ROOT
+go mod vendor
+cd $GOPATH/src
+mkdir -p github.com/aboodman
+ln -s $ROOT github.com/aboodman/replicant
+cd github.com/aboodman/replicant/repm
+rm repm.aar
+rm -rf Repm.framework
+gomobile bind --target=ios
+gomobile bind --target=android
+zip -r Repm.framework.zip Remp.framework
