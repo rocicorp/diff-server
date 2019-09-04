cf7f16b7400dfb7f57f77ae54d469ecd1d1dc2c4
Aaron Boodman <aaron@aaronboodman.com>
2019-09-03 16:22:17 -1000

    Cleanup

--- tool/release.sh
+++ tool/release.sh
@@ -3,10 +3,11 @@ ORIG=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT=$DIR/../
 cd $ROOT
-go mod vendor
+set -x
+go mod vendor > /dev/null 2>&1
 cd $GOPATH/src
 mkdir -p github.com/aboodman
-ln -s $ROOT github.com/aboodman/replicant
+ln -s $ROOT github.com/aboodman/replicant > /dev/null 2>&1 
 cd github.com/aboodman/replicant
 rm -rf build
 mkdir build
@@ -16,5 +17,5 @@ gomobile bind -ldflags="-s -w" --target=android ../repm/
 cp -R ../bind/flutter replicant-flutter-sdk
 rm -rf replicant-flutter-sdk/ios/Repm.framework
 cp -R Repm.framework replicant-flutter-sdk/ios/
-cp remp.aar replicant-flutter-sdk/android/
+cp repm.aar replicant-flutter-sdk/android/
 zip -r replicant-flutter-sdk.zip replicant-flutter-sdk
