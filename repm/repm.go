48de45505ccdc330936ad028f753aaaa3ca54a02
Aaron Boodman <aaron@aaronboodman.com>
2019-10-01 13:06:12 -1000

    Rework repm to be completely stateless. Also pull more logic (like clientID) into this. Much easier to work on either Golang or Dart. Really painful to work on iOS/Android, so thin those out.

--- repm/repm.go
+++ repm/repm.go
@@ -2,9 +2,12 @@
 package repm
 
 import (
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"runtime/debug"
 
 	"github.com/attic-labs/noms/go/spec"
@@ -14,22 +17,41 @@ import (
 	rlog "github.com/aboodman/replicant/util/log"
 )
 
+// Connection is a single open connection to Replicant.
 type Connection struct {
-	api    *api.API
-	dir    string
-	origin string
-	tmpDir string
+	api *api.API
+	dir string
 }
 
-func Open(dir, origin, tmpDir string) (*Connection, error) {
+// Open a replicant database. The replicantRootDir is a directory that contains
+// zero or more named databases. Origin is the origin to use for all write
+// transactions executed by this connection. tmpDir can be the empty string, in
+// which case the OS default temp directory will be used.
+//
+// If the named database doesn't exist it is created. If the specified root
+// directory doesn't exist, it is created.
+func Open(replicantRootDir, dbName, origin, tmpDir string) (*Connection, error) {
 	rlog.Init(os.Stderr, rlog.Options{Prefix: true})
 
-	log.Printf("Opening Replicant database at: %s for origin: %s", dir, origin)
+	if replicantRootDir == "" {
+		return nil, errors.New("replicantRootDir must be non-empty")
+	}
+
+	if dbName == "" {
+		return nil, errors.New("dbName must be non-empty")
+	}
+
+	if origin == "" {
+		return nil, errors.New("origin must be non-empty")
+	}
+
+	dbPath := path.Join(replicantRootDir, base64.RawURLEncoding.EncodeToString([]byte(dbName)))
+	log.Printf("Opening Replicant database '%s' at '%s' for origin '%s'", dbName, dbPath, origin)
 	if tmpDir != "" {
 		os.Setenv("TMPDIR", tmpDir)
 	}
 	log.Println("Using tempdir: ", os.TempDir())
-	sp, err := spec.ForDatabase(dir)
+	sp, err := spec.ForDatabase(dbPath)
 	if err != nil {
 		return nil, err
 	}
@@ -37,9 +59,11 @@ func Open(dir, origin, tmpDir string) (*Connection, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Connection{api: api.New(db), dir: dir, origin: origin, tmpDir: tmpDir}, nil
+	return &Connection{api: api.New(db), dir: dbPath}, nil
 }
 
+// Dispatch send an API request to Replicant, JSON-serialized parameters, and returns the response.
+// For the list of supported API requests and their parameters, see the api package.
 func (conn *Connection) Dispatch(rpc string, data []byte) (ret []byte, err error) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -51,7 +75,7 @@ func (conn *Connection) Dispatch(rpc string, data []byte) (ret []byte, err error
 			}
 			log.Printf("Replicant panicked with: %s\n%s\n", msg, string(debug.Stack()))
 			ret = nil
-			err = fmt.Errorf("Replicant panicked with: %s - see stderr for more.", msg)
+			err = fmt.Errorf("Replicant panicked with: %s - see stderr for more", msg)
 		}
 	}()
 	switch rpc {
@@ -68,10 +92,6 @@ func (conn *Connection) dropDatabase() error {
 	if err != nil {
 		return err
 	}
-	newConn, err := Open(conn.dir, conn.origin, conn.tmpDir)
-	if err != nil {
-		return err
-	}
-	*conn = *newConn
+	*conn = Connection{}
 	return nil
 }
--- repm/repm_test.go
+++ repm/repm_test.go
@@ -1,6 +1,7 @@
 package repm
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -16,7 +17,7 @@ func TestBasic(t *testing.T) {
 	assert := assert.New(t)
 	dir, err := ioutil.TempDir("", "")
 	assert.NoError(err)
-	conn, err := Open(dir, "test", "")
+	conn, err := Open(dir, "test", "test", "")
 	assert.NoError(err)
 	resp, err := conn.Dispatch("put", []byte(`{"id": "foo", "value": "bar"}`))
 	assert.Equal(`{"root":"3aktuu35stgss7djb5famn6u7iul32nv"}`, string(resp))
@@ -25,7 +26,7 @@ func TestBasic(t *testing.T) {
 	assert.Equal(`{"has":true,"value":"bar"}`, string(resp))
 	resp, err = conn.Dispatch("del", []byte(`{"id": "foo"}`))
 	assert.Equal(`{"ok":true,"root":"d3dqs6rctj3bmqg43pctpe9jol01h5kl"}`, string(resp))
-	testFile, err := ioutil.TempFile(dir, "")
+	testFile, err := ioutil.TempFile(conn.dir, "")
 	assert.NoError(err)
 
 	resp, err = conn.Dispatch("dropDatabase", nil)
@@ -34,4 +35,30 @@ func TestBasic(t *testing.T) {
 	fi, err := os.Stat(testFile.Name())
 	assert.Equal(nil, fi)
 	assert.True(os.IsNotExist(err))
+}
+
+func TestInvalidOpen(t *testing.T) {
+	assert := assert.New(t)
+
+	tc := []struct {
+		f  func() (*Connection, error)
+		ee string
+	}{
+		{func() (*Connection, error) { return Open("", "", "", "") }, "replicantRootDir must be non-empty"},
+		{func() (*Connection, error) { return Open("foo", "", "", "") }, "dbName must be non-empty"},
+		{func() (*Connection, error) { return Open("foo", "bar", "", "") }, "origin must be non-empty"},
+		{func() (*Connection, error) { return Open("foo", "bar", "baz", "") }, ""},
+	}
+
+	for i, t := range tc {
+		msg := fmt.Sprintf("test case %d", i)
+		c, err := t.f()
+		if t.ee != "" {
+			assert.EqualError(err, t.ee, msg)
+			assert.Nil(c, msg)
+		} else {
+			assert.NoError(err, msg)
+			assert.NotNil(c, msg)
+		}
+	}
 }
