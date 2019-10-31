fc23c63bcc83dc5b3d254a6a1644ce4b1bfc8cc9
Aaron Boodman <aaron@aaronboodman.com>
2019-10-30 23:39:02 -1000

    samples/flutter/redo: update database location and auth

--- samples/flutter/redo/lib/settings.dart
+++ samples/flutter/redo/lib/settings.dart
@@ -1,1 +1,2 @@
-String db = "https://replicate.to/serve/aa-todo";
+String db = "https://replicate.to/serve/1/sample-redo-flutter";
+String auth = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYiI6InNhbXBsZS1yZWRvLWZsdXR0ZXIiLCJleHAiOjI1NzI0Mjg4OTd9.QFZknRsnW9Qv4wZ9lHAkc7H5VpdW0BEuH9WgHaOGAZwFDnPpdQMgQvwHRn3tFtL2hVmZfcNg8TstPDz6aEPBnA";
