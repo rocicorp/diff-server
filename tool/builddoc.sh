c8ad8f29a9b54c473c99095af5f82c01827732ca
Aaron Boodman <aaron@aaronboodman.com>
2019-11-02 15:51:18 -1000

    remove builddoc.sh

--- tool/builddoc.sh
+++
@@ -1,4 +0,0 @@
-dartdoc \
---exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:io,dart:isolate,dart:math,dart:typed_data,dart:ui' \
---input $HOME/work/replicant/bind/flutter \
---output $HOME/work/replicant/doc/flutter --no-include-source 
