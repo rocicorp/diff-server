17cef4399d2e5f51a13dc11c58a36240e51748f1
Aaron Boodman <aaron@aaronboodman.com>
2020-05-15 11:03:51 -1000

    Add log.SetLogLevelFromString utility function.

--- util/log/log.go
+++ util/log/log.go
@@ -1,6 +1,8 @@
 package log
 
 import (
+	"fmt"
+
 	zl "github.com/rs/zerolog"
 	zlog "github.com/rs/zerolog/log"
 
@@ -9,4 +11,18 @@ import (
 
 func Default() zl.Logger {
 	return zlog.Hook(gid.ZLogHook{}).With().Timestamp().Logger()
+}
+
+func SetGlobalLevelFromString(s string) error {
+	switch s {
+	case "debug":
+		zl.SetGlobalLevel(zl.DebugLevel)
+	case "info":
+		zl.SetGlobalLevel(zl.InfoLevel)
+	case "error":
+		zl.SetGlobalLevel(zl.ErrorLevel)
+	default:
+		return fmt.Errorf("Unknown log level: %s", s)
+	}
+	return nil
 }
