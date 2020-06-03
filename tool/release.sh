2bf9317ae2b07e170215a33bcb71f787a1b558b1
Aaron Boodman <aaron@aaronboodman.com>
2020-06-02 14:06:38 -1000

    Build script changes

--- tool/release.sh
+++ tool/release.sh
@@ -6,25 +6,21 @@ set -x
 
 DIFFS_VERSION=`git describe --tags`
 
-rm -rf build-*
-BUILDDIR=build-${DIFFS_VERSION}
-mkdir $BUILDDIR
-mkdir ${BUILDDIR}/osx
-mkdir ${BUILDDIR}/linux
+BUILDDIR=build
+rm -rf build
+mkdir build
 
 cd $ROOT
 
 # diffs
 echo "Building diffs..."
 
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X roci.dev/replicant/util/version.v=$DIFFS_VERSION" -o ${BUILDDIR}/osx/diffs ./cmd/diffs
-GOOS=linux GOARCH=amd64 go build -ldflags "-X roci.dev/replicant/util/version.v=$DIFFS_VERSION" -o ${BUILDDIR}/linux/diffs ./cmd/diffs
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X roci.dev/replicant/util/version.v=$DIFFS_VERSION" -o build/diffs-osx ./cmd/diffs
+GOOS=linux GOARCH=amd64 go build -ldflags "-X roci.dev/replicant/util/version.v=$DIFFS_VERSION" -o build/diffs-linux ./cmd/diffs
 
 # noms tool
 echo "Building noms..."
 NOMS_VERSION=`go mod graph | grep '^github.com/attic-labs/noms@' | cut -d' ' -f1 | head -n1`
 go get $NOMS_VERSION
-GOOS=darwin GOARCH=amd64 go build -o ${BUILDDIR}/osx/noms github.com/attic-labs/noms/cmd/noms
-GOOS=linux GOARCH=amd64 go build -o ${BUILDDIR}/linux/noms github.com/attic-labs/noms/cmd/noms
-
-zip -r diffs-${DIFFS_VERSION}.zip $BUILDDIR
+GOOS=darwin GOARCH=amd64 go build -o build/noms-osx github.com/attic-labs/noms/cmd/noms
+GOOS=linux GOARCH=amd64 go build -o build/noms-linux github.com/attic-labs/noms/cmd/noms
